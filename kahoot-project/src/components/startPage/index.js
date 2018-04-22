4a7d14df60a63b442f91c5a45d8e7fff796e1cb9
Yura Serdyuk <pc@MacBook-Air-Yura.local>
2018-04-22 14:32:59 +0300

    create startPage

---
+++ kahoot-project/src/components/startPage/index.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+import React from 'react';
+class StartPage extends Component{
+ render(){
+     return(
+         <div className='startPage' >
+   <p>test from startpage</p>
+
+         </div>
+     )
+ }
+}
+
+
+
+export default StartPage;
+
