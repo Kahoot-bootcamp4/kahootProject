949246d0aa230773e401da1c5a532ac31da2f051
Yura Serdyuk <pc@MacBook-Air-Yura.local>
2018-04-23 13:45:05 +0300

    add component startPage

--- kahoot-project/src/App.js
+++ kahoot-project/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import StartPage from "./components/startPage";
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+          <StartPage/>
 
       </div>
     );
--- kahoot-project/src/components/startPage/index.js
+++ kahoot-project/src/components/startPage/index.js
@@ -1,17 +1,33 @@
 import {Component} from 'react';
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    display: block;
+    width: 100%;
+    height: 100vh;
+    background-color: #808080;
+
+`;
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+`;
+
+
 class StartPage extends Component{
+
  render(){
      return(
-         <div className='startPage' >
-   <p>test from startpage</p>
-
-         </div>
+         <Div>
+             <Button>sfsdf</Button>
+         </Div>
      )
  }
 }
-
-
-
 export default StartPage;
 
