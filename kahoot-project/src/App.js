e48c1814d072d29669795fbaaf0e11b6e6ad5b23
Oleksandr Chornyi <oleksandr.chornyi.23@gmail.com>
2018-04-23 14:16:23 +0300

    first commit

--- kahoot-project/src/App.js
+++ kahoot-project/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AdminLogin from './components/admin_login/Login'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AdminLogin/>
 
 
       </div>
