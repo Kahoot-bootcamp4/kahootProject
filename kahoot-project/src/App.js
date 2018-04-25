d53a9affe27cde17d5e23d5f651df196d5387018
Yura Serdiuk <cb8593bc@gmail.com>
2018-04-25 17:52:17 +0300

    create branch

--- kahoot-project/src/App.js
+++ kahoot-project/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AdminChoiseTest from "./components/adminChoiseTest/AdminChoiseTest";
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+<AdminChoiseTest/>
 
       </div>
     );
