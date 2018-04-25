825355b3d4f60ac6562c2af972e0130ec5d867c5
go17web@gmail.com <go17web@gmail.com>
2018-04-25 16:09:43 +0300

    govnokod-second

--- kahoot-project/src/components/admin_create_game/index.js
+++ kahoot-project/src/components/admin_create_game/index.js
@@ -14,31 +14,39 @@ class Create extends Component {
             edit: false,
         };
     }
+
     edit = () => {
         this.setState ({edit: true});
     };
     remove = () => {
-        alert ("Нажата кнопка удалить");
+        // alert ("Нажата кнопка удалить");
         this.props.deletBlock(this.props.index)
     };
     save = () => {
         this.props.updateQuestion (this.refs.newTxt0.value, this.props.index);
-        this.props.updateAnswers1 (this.refs.newTxt1.value, this.props.index);
-        this.props.updateAnswers2 (this.refs.newTxt2.value, this.props.index);
-        this.props.updateAnswers3 (this.refs.newTxt3.value, this.props.index);
-        this.props.updateAnswers4 (this.refs.newTxt4.value, this.props.index);
+        // this.props.updateAnswers1 (this.refs.`${answer}`.value, this.props.index);
+
+        // this.props.updateAnswers2 (this.refs.newTxt2.value, this.props.index);
+        // this.props.updateAnswers3 (this.refs.newTxt3.value, this.props.index);
+        // this.props.updateAnswers4 (this.refs.newTxt4.value, this.props.index);
+
+        this.props.toggle (this.props.index, this.props.j);
         this.setState ({edit: false})
-        // let value = this.refs.newTxt.value;
     };
 
     rendNorm = () => {
         return (
             <div className="box">
                 <div className="text">{this.props.children[0]}</div>
-                <div className="text">{this.props.children[1]}</div>
-                <div className="text">{this.props.children[2]}</div>
-                <div className="text">{this.props.children[3]}</div>
-                <div className="text">{this.props.children[4]}</div>
+                {this.props.children[1].map((answer) => {
+                    return (<div className="text">{answer}</div>)
+                })}
+
+                {/*<div className="text">{this.props.children[1]}</div>*/}
+                {/*<div className="text">{this.props.children[2]}</div>*/}
+                {/*<div className="text">{this.props.children[3]}</div>*/}
+                {/*<div className="text">{this.props.children[4]}</div>*/}
+
                 <Button height="30" width="70" onClick={this.edit} >Редактировать</Button>
                 <Button height="30" width="70" onClick={this.remove} >Удалить</Button>
             </div>
@@ -48,11 +56,38 @@ class Create extends Component {
     rendEdit = () => {
         return (
             <div className="box">
+
                 <textarea ref="newTxt0" defaultValue={this.props.children[0]}></textarea>
-                <textarea ref="newTxt1" defaultValue={this.props.children[1]}></textarea>
-                <textarea ref="newTxt2" defaultValue={this.props.children[2]}></textarea>
-                <textarea ref="newTxt3" defaultValue={this.props.children[3]}></textarea>
-                <textarea ref="newTxt4" defaultValue={this.props.children[4]}></textarea>
+
+                <div>
+                    {this.props.children[1].map((answer, j) => {
+                        return (
+                        <div>
+                            <textarea ref={answer} defaultValue={answer}/>
+                            <input type="checkbox" onClick={this.props.toggle} />
+                        </div>
+                        )
+                    })}
+                </div>
+
+
+
+                {/*<div>*/}
+                    {/*<input type="checkbox" onClick={this.toggle} />*/}
+                    {/*<textarea ref="newTxt1" defaultValue={this.props.children[1]}></textarea>*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                    {/*<input type="checkbox" onClick={this.toggle} />*/}
+                    {/*<textarea ref="newTxt2" defaultValue={this.props.children[2]}></textarea>*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                    {/*<input type="checkbox" onClick={this.toggle} />*/}
+                    {/*<textarea ref="newTxt3" defaultValue={this.props.children[3]}></textarea>*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                    {/*<input type="checkbox" onClick={this.toggle} />*/}
+                    {/*<textarea ref="newTxt4" defaultValue={this.props.children[4]}></textarea>*/}
+                {/*</div>*/}
                 <Button onClick={this.save} >Сохранить</Button>
             </div>
         );
@@ -91,24 +126,27 @@ class AdminCreateGame extends Component {
             games: [
                 { question:"Вопрос 1",
                     answers:[
-                        {var1:"v1",correct: false},
-                        {var2:"v2",correct: false},
-                        {var3:"v3",correct: false} ,
-                        {var4:"v4",correct: false}]
+                        {var:"1v1",correct: false},
+                        {var:"1v2",correct: false},
+                        {var:"1v3",correct: false},
+                        {var:"1v4",correct: false}
+                            ]
                 },
                 { question:"Вопрос 2",
                     answers:[
-                        {var1:"v1",correct: false},
-                        {var2:"v2",correct: false},
-                        {var3:"v3",correct: false} ,
-                        {var4:"v4",correct: false}]
+                        {var:"2v1",correct: false},
+                        {var:"2v2",correct: false},
+                        {var:"2v3",correct: false},
+                        {var:"2v4",correct: false}
+                            ]
                 },
                 { question:"Вопрос 3",
                     answers:[
-                        {var1:"v1",correct: false},
-                        {var2:"v2",correct: false},
-                        {var3:"v3",correct: false} ,
-                        {var4:"v4",correct: false}]
+                        {var:"3v1",correct: false},
+                        {var:"3v2",correct: false},
+                        {var:"3v3",correct: false},
+                        {var:"3v4",correct: false}
+                            ]
                 }
                   ]
         };
@@ -124,25 +162,32 @@ class AdminCreateGame extends Component {
         questionArr[i].question = text;
         this.setState({games:questionArr})
     };
-    updateAnswers1 = (text, i)=>{
+
+    updateAnswers1 = (text,i,j)=>{
         let questionArr = this.state.games;
-        questionArr[i].answers[0].var1 = text;
+        questionArr[i].answers[j].var = text;
         this.setState({games:questionArr})
     };
-    updateAnswers2 = (text, i)=>{
-        let questionArr = this.state.games;
-        questionArr[i].answers[1].var2 = text;
-        this.setState({games:questionArr})
-    };
-    updateAnswers3 = (text, i)=>{
-        let questionArr = this.state.games;
-        questionArr[i].answers[2].var3 = text;
-        this.setState({games:questionArr})
-    };
-    updateAnswers4 = (text, i)=>{
-        let questionArr = this.state.games;
-        questionArr[i].answers[3].var4 = text;
-        this.setState({games:questionArr})
+
+    // updateAnswers2 = (text, i)=>{
+    //     let questionArr = this.state.games;
+    //     questionArr[i].answers[1].var2 = text;
+    //     this.setState({games:questionArr})
+    // };
+    // updateAnswers3 = (text, i)=>{
+    //     let questionArr = this.state.games;
+    //     questionArr[i].answers[2].var3 = text;
+    //     this.setState({games:questionArr})
+    // };
+    // updateAnswers4 = (text, i)=>{
+    //     let questionArr = this.state.games;
+    //     questionArr[i].answers[3].var4 = text;
+    //     this.setState({games:questionArr})
+    // };
+    toggle = (i,j) => {
+        let corrects = this.state.games;
+        corrects[i].answers[j].correct = !corrects[i].answers[j].correct ;
+        console.log(this.state)
     };
 
 
@@ -152,24 +197,31 @@ class AdminCreateGame extends Component {
                 <Wrapper>
 
                     <p>СОЗДАНИЕ ИГРЫ</p>
-
-
                     {
                         this.state.games.map((item, i)=>{
                             return(
-                                <Create key={i} index={i}
+                                <Create  index={i}
                                         updateQuestion={this.updateQuestion}
                                         updateAnswers1={this.updateAnswers1}
-                                        updateAnswers2={this.updateAnswers2}
-                                        updateAnswers3={this.updateAnswers3}
-                                        updateAnswers4={this.updateAnswers4}
+
+                                        // updateAnswers2={this.updateAnswers2}
+                                        // updateAnswers3={this.updateAnswers3}
+                                        // updateAnswers4={this.updateAnswers4}
                                         deletBlock={this.deletBlock}
+                                        toggle={this.toggle}
                                 >
                                     {item.question}
-                                    {item.answers[0].var1}
-                                    {item.answers[1].var2}
-                                    {item.answers[2].var3}
-                                    {item.answers[3].var4}
+                                    { item.answers.map((answer, j)=>{
+                                        return (
+                                             answer.var+j
+                                         // console.log(answer.var)
+                                    )
+                                    }) }
+
+                                    {/*{item.answers[0].var1}*/}
+                                    {/*{item.answers[1].var2}*/}
+                                    {/*{item.answers[2].var3}*/}
+                                    {/*{item.answers[3].var4}*/}
                                     </Create>
                             )
                         })
