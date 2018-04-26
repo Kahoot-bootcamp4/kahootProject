e56130b91cafcc173485f6229d91b0a1b63c6e45
pandakung-fu <gritsak5@gmail.com>
2018-04-26 13:12:43 +0300

    start

--- kahoot-project/src/components/User_Name/UserName.js
+++ kahoot-project/src/components/User_Name/UserName.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 
-const UserName = (props) => {
-    return (
-        <div onClick={props.doneExercise}>
-            <input type="checkbox" checked={props.checked}/>
-            <span className={props.checked? 'checked': ''}>{props.name}</span>
-        </div>
-    )
+
+
+export default class UserName extends Component {
+    state = {
+        login: '',
+        password: ''
+    }
+    changeInput(field, e){
+        this.setState({
+            [field]: e.target.value
+        })
+    }
+
+    render() {
+        const {login, password} = this.state;
+        return (
+            <div class="root">
+                <input type="password"
+                       className="login__name"
+                       placeholder="Password"
+                       value={password}
+                       onChange={this.changeInput.bind(this, 'password')}
+                />
+                <br/>
+                <button onClick={this.login}>Enter</button>
+            </div>
+        )
+    }
 }
-export default UserName
+
+
+
--- kahoot-project/src/components/User_Start_Page/UserStartPage.js
+++ kahoot-project/src/components/User_Start_Page/UserStartPage.js
@@ -1,1 +1,41 @@
 import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import styled from 'styled-components';
+
+
+const test = styled.div
+    `background-color: red`
+
+
+
+
+export default class UserStartPage extends Component {
+    state = {
+        login: '',
+        password: ''
+    }
+
+    changeInput(field, e) {
+        this.setState({
+            [field]: e.target.value
+        })
+    }
+
+
+    render() {
+        const {login, password} = this.state;
+
+        return (
+            <div class="root">
+                <input type="text"
+                       className="login__name"
+                       placeholder="Login"
+                       value={login}
+                       onChange={this.changeInput.bind(this, 'login')}
+                />
+               <br/>
+                <button onClick={this.login}>Enter</button>
+            </div>
+        )
+    }
+}
