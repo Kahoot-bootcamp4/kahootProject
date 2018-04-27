e86ae089ad5f7e7245f8b72023ed7493c8e869a5
Gleb Leksikov <glebikov@gmail.com>
2018-04-27 16:02:01 +0300

    first draft

--- kahoot-project/src/App.js
+++ kahoot-project/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PendingRoom from './components/PendingRoom/';
 import './App.css';
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-
+        <PendingRoom />
       </div>
     );
   }
