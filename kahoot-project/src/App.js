227a71333d284a5e58906a591199558efe2bf97f
Dmitriy Opich <spooon1993@gmail.com>
2018-04-30 12:59:41 +0300

    add router in app

--- kahoot-project/src/App.js
+++ kahoot-project/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import Test from './test.js';
 
+import AdminCreateGame from './components/admin_create_game';
+import UserTestingPage from './components/users_testing_page';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Test/>
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Switch>
 
-      </div>
-    );
-  }
+                        <Route path='/create' component={AdminCreateGame} />
+                        <Route path='/' component={UserTestingPage} />
+
+                        
+                        {/*<Test/>*/}
+
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
-  
