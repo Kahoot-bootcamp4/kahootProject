1a3b6da46b7afbe394006f550cb3092c3ecf6a65
Dmitriy Opich <spooon1993@gmail.com>
2018-04-30 12:45:15 +0300

    merge commit

--- kahoot-project/src/App.js
+++ kahoot-project/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import './App.css';
-
+import Test from './test.js';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Test/>
 
       </div>
     );
@@ -16,3 +16,4 @@ class App extends Component {
 }
 
 export default App;
+  
