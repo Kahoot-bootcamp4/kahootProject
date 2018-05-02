b2746a260a19d198eba007cbe58133364175c65e
Yura Serdiuk <cb8593bc@gmail.com>
2018-05-02 17:02:12 +0300

    add style

--- kahoot-project/src/components/adminChoiseTest/AdminChoiseTest.js
+++ kahoot-project/src/components/adminChoiseTest/AdminChoiseTest.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import  {Button} from "../UI";
 
 const Wraper = styled.div`
-display:block;
+    padding-top: 100px;
+    display:block;
     width: 100%;
     height: 100vh;
     background-color: #2dbcc34d;
@@ -12,15 +13,20 @@ display:block;
 const Li = styled.li`
     border: 1px solid #000;
     margin-bottom: 20px;
+        display: flex;
+    justify-content: space-around;
 `;
 const Ul = styled.ul`
+    margin-top: 100px;
     list-style: none;
 `;
 const P  = styled.p`
     font-size: 25px;
     line-height: 0px;
     font-weight: 600;
-    color: #7e2aa7;`;
+    color: #7e2aa7;
+    width: 397.2px;
+        `;
 
 
 class AdminChoiseTest extends Component{
