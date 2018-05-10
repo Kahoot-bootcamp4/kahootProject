23a2ee33adf431527518d9f98066664a8355b877
pandakung-fu <gritsak5@gmail.com>
2018-05-10 14:06:05 +0300

    add button fix styles

--- kahoot-project/src/App.js
+++ kahoot-project/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AdminUser from "./components/Dashbord";
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+<AdminUser />
 
       </div>
     );
