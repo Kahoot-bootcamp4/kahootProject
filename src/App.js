bbaeee1efa93957ba673acc3b8d89a01203a1f5b
Spooon <spoon1993@gmail.com>
2019-05-09 17:15:32 +0300

    - add sass, css modules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch, NavLink, Redirect} from 'react-router-dom';
 import './App.css';
 import {excludeProp, getLogged} from "./utils";
@@ -20,60 +20,59 @@ import Common from './components/pages/Common/index';
 
 const PrivateRoute = (props) => {
 
-    // Проверяем залогинен ли пользователь
-    if (getLogged()){
+  // Проверяем залогинен ли пользователь
+  if (getLogged()) {
 
-        // Перенаправляем на закрытый роутинг
+    // Перенаправляем на закрытый роутинг
 
-        return <Route {...excludeProp(props, 'component')} component={props.component} />
-    } else {
+    return <Route {...excludeProp(props, 'component')} component={props.component}/>
+  } else {
 
-        // Перенаправляем на страницу логина
-        return <Redirect to="/login" />
-    }
+    // Перенаправляем на страницу логина
+    return <Redirect to="/login"/>
+  }
 };
 
 
 class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-            <Router>
-            <div className="App">
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
             <div className='links'>
-            <NavLink to='/'>/==start==/</NavLink>
-        <NavLink to='/common/testing'>Test</NavLink>
-            <NavLink to='/create'>create</NavLink>
-            <NavLink to='/edit'>edit</NavLink>
-            <NavLink to='/login'>Login</NavLink>
-            <NavLink to='/start'>Start</NavLink>
-            <NavLink to='/name'>Name</NavLink>
-            <NavLink to='/common/result'>result</NavLink>
-            <NavLink to='/common/pending'>pending</NavLink>
-            <NavLink to='/choise'>choise</NavLink>
-            <NavLink to='/common/dashbord'>dashbord</NavLink>
-            <NavLink to='/common'>Common</NavLink>
+              <NavLink to='/'>/==start==/</NavLink>
+              <NavLink to='/common/testing'>Test</NavLink>
+              <NavLink to='/create'>create</NavLink>
+              <NavLink to='/edit'>edit</NavLink>
+              <NavLink to='/login'>Login</NavLink>
+              <NavLink to='/start'>Start</NavLink>
+              <NavLink to='/name'>Name</NavLink>
+              <NavLink to='/common/result'>result</NavLink>
+              <NavLink to='/common/pending'>pending</NavLink>
+              <NavLink to='/choise'>choise</NavLink>
+              <NavLink to='/common/dashbord'>dashbord</NavLink>
+              <NavLink to='/common'>Common</NavLink>
             </div>
 
             <Switch>
-            <Route path='/login' component={AdminLogin} />
-        <Route path='/start' component={UserStartPage} />
-        <Route path='/name' component={UserName} />
-        <PrivateRoute path='/create' component={AdminCreateGame} />
-        <PrivateRoute path='/edit/:id' component={AdminEditGame} />
-        <Route path='/common/' component={Common} />
-        <Route path='/choise' component={AdminChoiseTest} />
+              <Route path='/login' component={AdminLogin}/>
+              <Route path='/start' component={UserStartPage}/>
+              <Route path='/name' component={UserName}/>
+              <PrivateRoute path='/create' component={AdminCreateGame}/>
+              <PrivateRoute path='/edit/:id' component={AdminEditGame}/>
+              <Route path='/common/' component={Common}/>
+              <Route path='/choise' component={AdminChoiseTest}/>
 
-        <Route path='/' component={StartPage} />
+              <Route path='/' component={StartPage}/>
 
 
-
-        </Switch>
-        </div>
+            </Switch>
+          </div>
         </Router>
-        </Provider>
+      </Provider>
     )
-    }
+  }
 }
 
 export default App;
